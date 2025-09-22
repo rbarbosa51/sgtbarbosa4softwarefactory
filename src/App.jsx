28761ef3db75c3bb376065e2fcebf9349dfabecf
Rafael Barbosa <rafaelbarbosa@Rafaels-MacBook-Pro.local>
2025-09-21 18:25:49 -0700

    GoogleFonts

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,13 @@ function App() {
   
 
   return (
-    <div className="h-dvh bg-purple-200">
-      <h1 className="text-5xl">Hello World</h1>
+    <div className="h-dvh bg-purple-200 flex flex-col items-center">
+      <h1 className="text-5xl font-avenir ">Hello World</h1>
+      <h1 className="text-5xl font-inconsolata ">Hello World</h1>
+      <h1 className="text-5xl font-italiana ">Hello World</h1>
+      <h1 className="text-5xl font-montserrat ">Hello World</h1>
+      <h1 className="text-5xl font-roboto ">Hello World</h1>
+      <h1 className="text-5xl font-waterbrush ">Hello World</h1>
     </div>
   )
 }
